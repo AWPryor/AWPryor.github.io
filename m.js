f728afd8aa2380d852251e48aa5598061a5879b0
AWPryor <55792412+AWPryor@users.noreply.github.com>
2024-05-14 22:50:43 +0100

    Update m.js

--- m.js
+++ m.js
@@ -1,5 +1,6 @@
 // Assume this is the content of the file hosted at https://ex.am/m
 console.log("Module loaded and executed!");
+alert('any XSS is possible here')
 
 
 // If you want to perform some specific function automatically, define and call it:
